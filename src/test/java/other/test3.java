b9a4bf4ae9675e2185011728c43bf28f489350a3
tian <tiannwei0629@126.com>
2019-04-05 19:43:48 +0800

    test3

---
+++ src/test/java/other/test3.java
@@ -0,0 +1,17 @@
+package other;
+
+import org.junit.Test;
+
+/**
+ * @author David Tian
+ * @since 2019-04-05
+ */
+public class test3 {
+    @Test
+    public void test1() {
+
+        System.out.println("=====================1========================");
+        System.out.println("=====================2========================");
+        System.out.println("=====================3========================");
+    }
+}
