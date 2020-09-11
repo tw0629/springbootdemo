072746aaf108afe33d8e2421f9632a6f0850ae62
tianwei <tianwei@hupu.com>
2020-09-11 08:00:16 +0800

    1.3

---
+++ src/main/java/com/concurrentRob/coupon2/CouponDemo.java
@@ -0,0 +1,49 @@
+package com.concurrentRob.coupon2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author David Tian
+ * @desc
+ * @since 2020-09-11 00:56
+ */
+public class CouponDemo {
+
+
+    public static void main(String[] args) {
+
+        Map<Integer, AtomicInteger> map = new HashMap<>();
+        map.put(0, new AtomicInteger(5));
+        map.put(1, new AtomicInteger(15));
+        map.put(2, new AtomicInteger(10));
+        map.put(3, new AtomicInteger(5));
+        map.put(4, new AtomicInteger(10));
+
+        //注意: 这里只能有InheritableThreadLocal,不能用ThreadLocal
+        InheritableThreadLocal itl = null;
+        for(int i=0;i<100;i++){
+            itl = new InheritableThreadLocal();
+            itl.set(i);
+            InheritableThreadLocal finalItl = itl;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Integer integer = (Integer) finalItl.get();
+                    AtomicInteger kucun = map.get(integer % 5);
+
+                    int zhangshu = kucun.decrementAndGet();
+                    if(zhangshu<0){
+                        System.out.println("路人"+integer+" 没抢到优惠券的类型: "+(integer % 5));
+                    }else {
+                        System.out.println("路人"+integer+" 抢购一张优惠券的类型: "+(integer % 5)+" 还剩:"+zhangshu+"张");
+                    }
+                }
+            }).start();
+        }
+
+
+
+    }
+}
